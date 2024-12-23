fd90cdfe62f8118d2586ada093f3bd6ac27e1f0a
eviala42 <viala.enzo@gmail.com>
2024-12-23 13:10:15 +0100

    nice

---
+++ cpp_09/ex00/includes/BitcoinExchange.hpp
@@ -0,0 +1,22 @@
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <ctime>
+#include <cstdlib>
+
+class BitcoinExchange {
+	public:
+
+	private:
+
+};
+
+#endif
